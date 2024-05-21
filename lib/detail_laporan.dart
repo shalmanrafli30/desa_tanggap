7f59aabb1a69e620e7814e6b19e6d11334ad70a3
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-05-22 02:29:10 +0700

    status laporan section

--- lib/detail_laporan.dart
+++ lib/detail_laporan.dart
@@ -27,14 +27,94 @@ class DetailLaporan extends StatelessWidget {
                 ClipRRect(
                   child: Image.asset(
                     "assets/sampah.jpeg",
-                    width: double.maxFinite,
+                    width: double.infinity,
                     height: 250, // Adjust the height as needed
                     fit: BoxFit.cover,
                   ),
                 ),
               ],
             ),
-            // Konten lainnya
+            // STATUS LAPORAN
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+              child: Container(
+                width: double.infinity,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(237, 242, 244, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      // Kolom untuk 'Nomor Laporan'
+                      const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Nomor Laporan',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Color.fromRGBO(167, 167, 167, 1),
+                                fontSize: 12),
+                          ),
+                          SizedBox(
+                              height: 8), // Add space between title and content
+                          SelectableText(
+                            'LPR220524001',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 15),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                      // Vertical Divider
+                      const VerticalDivider(
+                        color: Color.fromRGBO(
+                            167, 167, 167, 1), // Adjust the color as needed
+                        thickness: 1, // Adjust the thickness as needed
+                        indent: 20, // Adjust the top spacing as needed
+                        endIndent: 20, // Adjust the bottom spacing as needed
+                      ),
+                      // Kolom untuk 'Status Laporan'
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Status Laporan',
+                            style: TextStyle(
+                                color: Color.fromRGBO(167, 167, 167, 1),
+                                fontSize: 12),
+                          ),
+                          const SizedBox(
+                              height: 8), // Add space between title and content
+                          Container(
+                            padding: const EdgeInsets.all(
+                                4), // Add padding around the text
+                            decoration: BoxDecoration(
+                              color: const Color.fromRGBO(168, 168, 168, 1),
+                              borderRadius: BorderRadius.circular(
+                                  4), // Optional: if you want rounded corners
+                            ),
+                            child: const Text(
+                              'Menunggu',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 15,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // END STATUS LAPORAN
           ],
         ),
       ),
